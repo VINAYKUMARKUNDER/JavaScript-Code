a9292546bac3e333dffec1c329e420994877b099
VINAYKUMARKUNDER <vinay3000111@gmail.com>
2023-04-10 09:23:36 +0530

    footer added

---
+++ AdobeFronted/components/footer.js
@@ -0,0 +1,13 @@
+function footer(){
+    `
+    <footer class="footer mt-auto py-3">
+    <div class="p-3 mb-2 bg-dark text-dark text-center">
+      <span class="text-muted">copyright &copy; 2023 aaxlejobz. All rights reserved.</span>
+    </div>
+  </footer>
+  
+    
+    `
+}
+
+export default footer()
